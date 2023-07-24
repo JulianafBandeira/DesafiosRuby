b81b0e6c01992193365b1f8f4155e27063fe52e4
JulianafBandeira <julianafbbarbosa@gmail.com>
2023-07-24 17:30:32 -0300

    desafios

---
+++ produto.rb
@@ -0,0 +1,8 @@
+puts"Digite um número"
+firstValue = gets.to_i;
+puts"Digite outro"
+secondValue = gets.to_i;
+
+PROD = firstValue * secondValue
+
+puts"PROD = #{PROD}"
---
+++ raio.rb
@@ -0,0 +1,7 @@
+PI = 3.14159
+puts"Informe um valor: "
+raio = gets().to_f
+
+VOLUME =  (4/3.0) * PI * raio**3
+
+puts"VOLUME = #{VOLUME}"
